9938e2c96049e9bd9e1a54e0b72a4469eeb59d66
Abhinav Mathur <raaaaj5000@yahoo.co.in>
2016-11-27 23:28:49 -0500

    added puma.rb

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,11 +8,11 @@ set :puma_process_name, 'staging'
 set :shared_path_app, '/var/www/staging.vivieu.com/shared'
 
 set :deploy_to, -> { fetch(:deploy_location) }
-set :puma_bind,       "unix://#{:shared_path_app}tmp/sockets/staging-puma.sock"
-set :puma_state,      "#{:shared_path_app}tmp/pids/puma.state"
-set :puma_pid,        "#{:shared_path_app}tmp/pids/puma.pid"
-set :puma_access_log, "#{:shared_path_app}log/puma.error.log"
-set :puma_error_log,  "#{:shared_path_app}log/puma.access.log"
+set :puma_bind,       "unix://#{:shared_path_app}/tmp/sockets/staging-puma.sock"
+set :puma_state,      "#{:shared_path_app}/tmp/pids/puma.state"
+set :puma_pid,        "#{:shared_path_app}/tmp/pids/puma.pid"
+set :puma_access_log, "#{:shared_path_app}/log/puma.error.log"
+set :puma_error_log,  "#{:shared_path_app}/log/puma.access.log"
 set :sidekiq_role, :app
 set :sidekiq_config, "#{:shared_app_path}/config/sidekiq.yml"
 set :sidekiq_env, 'staging'
