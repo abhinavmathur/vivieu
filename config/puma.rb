9ce2616e40cb8036a4f238defe32e0fdce37db40
Abhinav Mathur <raaaaj5000@yahoo.co.in>
2016-11-27 23:36:20 -0500

    fixed puma file

--- config/puma.rb
+++ config/puma.rb
@@ -10,12 +10,12 @@ environment ENV['RAILS_ENV']
 daemonize   true
 
 if ENV['RAILS_ENV'] == 'staging'
-  bind "unix:#{deploy_staging}/shared/tmp/sockets/staging-puma.sock"
-  pidfile "#{deploy_staging}/shared/tmp/pids/puma.pid"
-  state_path "#{deploy_staging}/shared/tmp/sockets/puma.state"
+  bind "unix:/var/www/staging.vivieu.com/shared/tmp/sockets/staging-puma.sock"
+  pidfile "/var/www/staging.vivieu.com/shared/tmp/pids/puma.pid"
+  state_path "/var/www/staging.vivieu.com/shared/tmp/sockets/puma.state"
 
-  stdout_redirect "#{deploy_staging}/shared/log/puma.stdout.log", "#{deploy_staging}/shared/log/puma.stderr.log", true
-  activate_control_app "unix:#{deploy_staging}/shared/tmp/sockets/pumactl.sock", { no_token: true }
+  stdout_redirect "/var/www/staging.vivieu.com/shared/log/puma.stdout.log", "#{deploy_staging}/shared/log/puma.stderr.log", true
+  activate_control_app "unix:/var/www/staging.vivieu.com/shared/tmp/sockets/pumactl.sock", { no_token: true }
 else
   bind "unix:#{deploy_production}/shared/tmp/sockets/production-puma.sock"
   pidfile "#{deploy_production}/shared/tmp/pids/puma.pid"
